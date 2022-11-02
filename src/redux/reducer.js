af1f1903e0ffcd287ec3741c70e3573f05791f9d
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-11-02 23:01:57 +0200

    upload

---
+++ src/redux/reducer.js
@@ -0,0 +1,41 @@
+import { createReducer } from '@reduxjs/toolkit';
+import {
+  addTask,
+  deleteTask,
+  setStatusFilter,
+  toggleCompleted,
+} from './actions';
+import { statusFilters } from './constants';
+
+const tasksInitialState = [
+  { id: 0, text: 'Learn HTML and CSS', completed: true },
+  { id: 1, text: 'Get good at JavaScript', completed: true },
+  { id: 2, text: 'Master React', completed: false },
+  { id: 3, text: 'Discover Redux', completed: false },
+  { id: 4, text: 'Build amazing apps', completed: false },
+];
+
+export const tasksReducer = createReducer(tasksInitialState, {
+  [addTask]: (state, action) => {
+    state.push(action.payload);
+  },
+  [deleteTask]: (state, action) => {
+    return state.filter(task => task.id !== action.payload);
+  },
+  [toggleCompleted]: (state, action) => {
+    return state.map(task => {
+      if (task.id !== action.payload) return task;
+      return { ...task, completed: !task.completed };
+    });
+  },
+});
+
+const filtersInitialState = {
+  status: statusFilters.all,
+};
+
+export const filtersReducer = createReducer(filtersInitialState, {
+  [setStatusFilter]: (state, action) => {
+    state.status = action.payload;
+  },
+});
---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { filtersReducer, tasksReducer } from './reducer';
+
+export const store = configureStore({
+  reducer: { tasks: tasksReducer, filters: filtersReducer },
+});
